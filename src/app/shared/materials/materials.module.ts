717d84ffb72df5e56c330684ed83c69039f538d1
JafetMo5601 <jafet.moraugalde15@gmail.com>
2021-10-10 22:19:17 -0600

    Create a new shared module to have all the Angular Material packages prepared for the modules.

---
+++ src/app/shared/materials/materials.module.ts
@@ -0,0 +1,39 @@
+import { MatButtonToggleModule } from '@angular/material/button-toggle';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatNativeDateModule } from '@angular/material/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
+import { MatListModule } from '@angular/material/list';
+import { MatIconModule } from '@angular/material/icon';
+import { MatCardModule } from '@angular/material/card';
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+  ], 
+  exports: [
+    MatButtonToggleModule,
+    MatSlideToggleModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatExpansionModule,
+    MatDividerModule,
+    MatSidenavModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatInputModule,
+    MatCardModule,
+    MatIconModule,
+    MatListModule,
+  ]
+})
+export class MaterialsModule { }
