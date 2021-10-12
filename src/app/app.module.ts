21a23b6bfd1bd7a91973675ea8953042b1cd36f8
JafetMo5601 <jafet.moraugalde15@gmail.com>
2021-10-11 22:00:03 -0600

    Change the background color of the project.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,11 @@ import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { NotFoundComponent } from './components/not-found/not-found.component';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SideBarComponent } from './components/dashboard/side-bar/side-bar.component';
+import { TopMenuComponent } from './components/dashboard/top-menu/top-menu.component';
+import { ContentComponent } from './components/dashboard/content/content.component';
+import { MaterialsModule } from './shared/materials/materials.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 
 @NgModule({
@@ -13,9 +18,14 @@ import { AppComponent } from './app.component';
     DashboardComponent,
     NotFoundComponent,
     AppComponent,
+    SideBarComponent,
+    TopMenuComponent,
+    ContentComponent,
   ],
   imports: [
+    BrowserAnimationsModule,
     AppRoutingModule,
+    MaterialsModule,
     BrowserModule,
     CommonModule,
   ],
