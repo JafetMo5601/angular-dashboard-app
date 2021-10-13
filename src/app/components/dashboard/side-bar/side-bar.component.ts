4e9387a2a55aba0ab40d85f6da729042a0c06d0c
JafetMo5601 <jafet.moraugalde15@gmail.com>
2021-10-12 23:00:58 -0600

    Improve the look and feel of the panels for the search, recently played and your games sections.

--- src/app/components/dashboard/side-bar/side-bar.component.ts
+++ src/app/components/dashboard/side-bar/side-bar.component.ts
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SideBarComponent implements OnInit {
 
+  placeholder = 'Search...'
+
   constructor() { }
 
   ngOnInit(): void {
