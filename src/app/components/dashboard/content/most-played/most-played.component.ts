00e2be43af76fd8274879b3eb8ec137bb36c39c8
JafetMo5601 <jafet.moraugalde15@gmail.com>
2021-10-13 22:04:54 -0600

    Finish the most played games dropdown list

---
+++ src/app/components/dashboard/content/most-played/most-played.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-most-played',
+  templateUrl: './most-played.component.html',
+  styleUrls: ['./most-played.component.css']
+})
+export class MostPlayedComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
