6bb0c0d9e7dce6e5f13365c568caec0ad599c164
JafetMo5601 <jafet.moraugalde15@gmail.com>
2021-10-13 22:07:59 -0600

    Add the new components created to the app module.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,25 +2,31 @@ import { BrowserModule } from '@angular/platform-browser';
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 
-import { DashboardComponent } from './components/dashboard/dashboard.component';
-import { NotFoundComponent } from './components/not-found/not-found.component';
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { MostPlayedComponent } from './components/dashboard/content/most-played/most-played.component';
+import { StatisticsComponent } from './components/dashboard/content/statistics/statistics.component';
+import { FriendsComponent } from './components/dashboard/content/friends/friends.component';
 import { SideBarComponent } from './components/dashboard/side-bar/side-bar.component';
 import { TopMenuComponent } from './components/dashboard/top-menu/top-menu.component';
 import { ContentComponent } from './components/dashboard/content/content.component';
-import { MaterialsModule } from './shared/materials/materials.module';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialsModule } from './shared/materials/materials.module';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
 
 
 @NgModule({
   declarations: [
+    StatisticsComponent,
+    MostPlayedComponent,
     DashboardComponent,
     NotFoundComponent,
-    AppComponent,
     SideBarComponent,
     TopMenuComponent,
     ContentComponent,
+    FriendsComponent,
+    AppComponent,
   ],
   imports: [
     BrowserAnimationsModule,
