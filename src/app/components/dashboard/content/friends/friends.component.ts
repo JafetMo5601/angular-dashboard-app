e9e8121e94ea315ef8e12df49548356d5df84e4e
JafetMo5601 <jafet.moraugalde15@gmail.com>
2021-10-17 17:03:19 -0600

    Finish the look and feel of the friend list component with the hardcoded data.

--- src/app/components/dashboard/content/friends/friends.component.ts
+++ src/app/components/dashboard/content/friends/friends.component.ts
@@ -6,6 +6,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./friends.component.css']
 })
 export class FriendsComponent implements OnInit {
+  friends: any[] = [
+    {
+      nickName: 'Juan Perez',
+      status: 'Playing...'
+    },
+    {
+      nickName: 'Juan Perez',
+      status: 'Playing...'
+    },
+    {
+      nickName: 'Juan Perez',
+      status: 'Playing...'
+    },
+    {
+      nickName: 'Juan Perez',
+      status: 'Last time 5min ago'
+    },
+    {
+      nickName: 'Juan Perez',
+      status: 'Playing...'
+    },
+    {
+      nickName: 'Juan Perez',
+      status: 'Last time 4d ago'
+    }
+  ] 
 
   constructor() { }
 
