2b1e60461400e257d410c295f246dec9ee6bfe71
JafetMo5601 <jafet.moraugalde15@gmail.com>
2021-10-23 15:34:51 -0600

    Add the charts of the statistics component.

--- src/app/components/dashboard/content/statistics/statistics.component.ts
+++ src/app/components/dashboard/content/statistics/statistics.component.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import { single } from './data';
 
 @Component({
   selector: 'app-statistics',
   templateUrl: './statistics.component.html',
   styleUrls: ['./statistics.component.css']
 })
-export class StatisticsComponent implements OnInit {
+export class StatisticsComponent {
 
-  constructor() { }
+  saleData = [
+    { name: "Mobiles", value: 105000 },
+    { name: "Laptop", value: 55000 },
+    { name: "AC", value: 15000 },
+    { name: "Headset", value: 150000 },
+    { name: "Fridge", value: 20000 }
+  ];
 
-  ngOnInit(): void {
-  }
+  
 
+  constructor() {}
 }
