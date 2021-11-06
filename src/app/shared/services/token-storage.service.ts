9810b2778b72cd47fa9155528fccfb0fe1a33328
JafetMo5601 <jafet.moraugalde15@gmail.com>
2021-11-06 17:01:03 -0600

    Fix the problems of the localstorage since the token was not being saved.

--- src/app/shared/services/token-storage.service.ts
+++ src/app/shared/services/token-storage.service.ts
@@ -2,22 +2,21 @@ import { Injectable } from '@angular/core';
 
 
 const TOKEN_KEY = 'auth-token';
-const USER_KEY = 'user-key';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TokenStorageService {
   signOut(): void {
-    window.sessionStorage.clear();
+    localStorage.clear();
   }
 
   public saveToken(token: string): void {
-    window.sessionStorage.removeItem(TOKEN_KEY);
-    window.sessionStorage.setItem(TOKEN_KEY, token);
+    localStorage.removeItem(TOKEN_KEY);
+    localStorage.setItem(TOKEN_KEY, token);
   }
 
   public getToken(): string | null {
-    return window.sessionStorage.getItem(TOKEN_KEY);
+    return localStorage.getItem(TOKEN_KEY);
   }
 }
